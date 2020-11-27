a396193d621a206ceb6ec950d6bc5dbf537f10ad
Austin Schrader <austin.jschrader@gmail.com>
2020-11-27 14:25:10 -0800

    Take the user input and filter the cars list to only show prices lower than user input

--- Car.cs
+++ Car.cs
@@ -7,10 +7,16 @@ public class Car
   public string MakeModel;
   public int Price;
   public int Miles;
+
+  public bool WorthBuying(int maxPrice)
+  {
+    return (Price <= maxPrice);
+  }
 }
 
 public class Program
 {
+
   public static void Main()
   {
     Car bmw = new Car();
@@ -35,9 +41,25 @@ public class Program
 
     List<Car> Cars = new List<Car>() { bmw, yugo, ford, amc };
 
+    Console.WriteLine("Enter maximum price: ");
+    string stringMaxPrice = Console.ReadLine();
+    int maxPrice = int.Parse(stringMaxPrice);
+
+    List<Car> CarsMatchingSearch = new List<Car>(0);
+
     foreach (Car automobile in Cars)
+    {
+      if (automobile.WorthBuying(maxPrice))
+      {
+        CarsMatchingSearch.Add(automobile);
+      }
+
+    }
+
+    foreach (Car automobile in CarsMatchingSearch)
     {
       Console.WriteLine(automobile.MakeModel);
     }
+
   }
 }

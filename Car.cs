0829f1c41bbbfa0f4591393a24966084b17b39ed
Austin Schrader <austin.jschrader@gmail.com>
2020-11-27 14:17:00 -0800

    Print all the hardcoded instances of Car

--- Car.cs
+++ Car.cs
@@ -1,12 +1,43 @@
 using System;
 
-
 using System.Collections.Generic;
 
 public class Car
 {
   public string MakeModel;
-  public string Price;
-  public string Miles;
+  public int Price;
+  public int Miles;
+}
 
+public class Program
+{
+  public static void Main()
+  {
+    Car bmw = new Car();
+    bmw.MakeModel = "2020 X5";
+    bmw.Price = 50000;
+    bmw.Miles = 10;
+
+    Car yugo = new Car();
+    yugo.MakeModel = "1980 Yugo Koral";
+    yugo.Price = 5000;
+    yugo.Miles = 56000;
+
+    Car ford = new Car();
+    ford.MakeModel = "1972 Ford Ram";
+    ford.Price = 1400;
+    ford.Miles = 230000;
+
+    Car amc = new Car();
+    amc.MakeModel = "1976 AMC Pacer";
+    amc.Price = 400;
+    amc.Miles = 198000;
+
+    List<Car> Cars = new List<Car>() { bmw, yugo, ford, amc };
+
+    foreach (Car automobile in Cars)
+    {
+      Console.WriteLine(automobile.MakeModel);
+    }
+  }
 }
